0cb1fe0c3bf8c52ea8971dc14a4bfe5ec4b44354
romkaimp <roman.150704@mail.ru>
2024-08-23 18:20:00 +0300

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,66 @@
 import ItemCard from "./components/ItemCard.jsx";
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import { Menu } from 'antd';
-const items = [
-  {
-    key: 'g1',
-    label: 'Список товаров',
-    type: 'group',
-    children: [
-      {
-        key: '1',
-        label: 'Option 1',
-      },
-      {
-        key: '2',
-        label: 'Option 2',
-      },
-    ],
-  },
+import axios from "axios";
+import { Menu , Spin} from 'antd';
 
-];
+
 const App = () => {
+  const [currencies, setCurrencies] = useState([])
+  const [itemName, setItemName] = useState('AK')
+  const [itemData, setItemData] = useState(null)
 
-  const fetchCurrensies = () => {
+  const fetchCurrencies = () => {
     axios.get('http://127.0.0.1:8000/predict').then(r => {
-        console.log("r", r)
+        const currencies = r.data
+        const menuItems = [
+          {
+            key: 'g1',
+            label: 'Список товаров',
+            type: 'group',
+            children: currencies.map(c => {
+            return {label: c.name, key: c.name}
+            })
+        }]
+        setCurrencies(menuItems)
     })
-    }
+  }
+
+  const fetchItem = () => {
+    axios.get(`http://127.0.0.1:8000/predict/${itemName}`).then(r => {
+        setItemData(r.data)
+    })
+  }
 
   useEffect(() => {
-    fetchCurrensies()
+    fetchCurrencies()
   }, []);
 
+  useEffect(() => {
+    setItemData(null)
+    fetchItem()
+  }, [itemName]);
+
   const onClick = (e) => {
-    console.log('click ', e);
+    setItemName(e.key)
   };
   return (
-    <Menu
-      onClick={onClick}
-      style={{
-        width: 256,
-      }}
-      defaultSelectedKeys={['1']}
-      defaultOpenKeys={['sub1']}
-      mode="inline"
-      items={items}
-    />
+    <div className="flex">
+        <Menu
+          onClick={onClick}
+          style={{
+            width: 256,
+          }}
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          items={currencies}
+          className="h-screen overflow-scroll"
+        />
+        <div className="mx-auto my-auto">
+            {itemData ? <ItemCard item={itemData}/> : <Spin size="large"/>}
+        </div>
+    </div>
   );
 };
 export default App;
--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,25 +1,37 @@
 import {Card} from "antd";
-function ItemCard() {
 
-
-  return (
-    <>
-    <Card
-      title={<div className="flex items-center gap-3">
-                <img src='https://community.akamai.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFQznaKdID5D6d23ldHSwKOmZeyEz21XvZZ12LzE9t6nigbgqkplNjihJIaLMlhpF1ZeR5c/62fx62f'></img>
-                <span>Walt</span>
-            </div>}
-      extra={<a href="#">More</a>}
-      style={{
-        width: 300,
-      }}
-    >
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
-    </>
-  )
+function ItemCard(props) {
+    const currency = props.item
+    const cost = currency.costs.at(-1)
+    let up;
+    if (currency.prediction.at(0).cost < currency.prediction.at(1).cost)
+        up = 'up'
+    else
+        up = 'down'
+    console.log(props)
+    const preds = [
+    Math.round(currency.prediction.at(0).cost * 100) / 100, 
+    Math.round(currency.prediction.at(1).cost * 100) / 100,
+    Math.round(currency.prediction.at(2).cost * 100) / 100
+    ]
+    return (
+        <>
+            <Card
+                title={<div className="flex items-center gap-3">
+                    <img src='https://community.akamai.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFQznaKdID5D6d23ldHSwKOmZeyEz21XvZZ12LzE9t6nigbgqkplNjihJIaLMlhpF1ZeR5c/62fx62f'></img>
+                    <span>{props.item.name}</span>
+                </div>}
+                extra={<a href="#">More</a>}
+                style={{
+                width: 300,
+            }}
+            >
+                <p>Последняя цена: {Math.round(cost.cost * 100) / 100}, время: {cost.time}</p>
+                <p>Предсказания: {up} {preds[0]}, {preds[1]}, {preds[2]}</p>
+                <p>Card content</p>
+            </Card>
+        </>
+    )
 }
 
 export default ItemCard
